15ac2f29ce00be427fb9792a3493cc040eece2ee
Om12ek99 <omar.elkamili@gmail.com>
2024-06-14 17:54:12 +0200

    add: updated scheeme to match the array given by the teacher

--- database/migrations/2024_06_14_143437_create_comics_table.php
+++ database/migrations/2024_06_14_143437_create_comics_table.php
@@ -11,31 +11,20 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('comics', function (Blueprint $table) {          //Crea una nuova tabella chiamata comics nel database. La funzione accetta una callback che riceve un'istanza di Blueprint utilizzata per definire le colonne della tabella.
+        Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('author');
             $table->text('description')->nullable();
-            $table->string('publisher');
-            $table->date('release_date');
+            $table->string('thumb')->nullable();
             $table->decimal('price', 8, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
 
 
-    //in MySql
-    // CREATE TABLE `comics` (
-    //     `id` bigint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    //     `title` varchar(255) NOT NULL,
-    //     `author` varchar(255) NOT NULL,
-    //     `description` text NULL,
-    //     `publisher` varchar(255) NOT NULL,
-    //     `release_date` date NOT NULL,
-    //     `price` decimal(8, 2) NOT NULL,
-    //     `created_at` timestamp NULL DEFAULT NULL,
-    //     `updated_at` timestamp NULL DEFAULT NULL
-    //   );
 
     /**
      * Reverse the migrations.
