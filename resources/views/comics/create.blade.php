ab3b9d5831d7735385962c32058bd6973ff88720
Om12ek99 <omar.elkamili@gmail.com>
2024-06-14 20:14:31 +0200

    add: created basic form in create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -7,5 +7,40 @@
 </head>
 <body>
     <h1>crea</h1>
+    <form 
+    method="post"
+    action="">
+
+    <!-- title -->
+    <div>
+        <label for="title">Inserisci un titolo</label>
+        <input type="text"  name="title" placeholder="titolo">
+    </div>
+    <!-- description -->
+    <div>
+        <label for="title">Inserisci una descrizione</label>
+        <input type="text"  name="desc" placeholder="descrizione">
+    </div>
+    <!-- prezzo -->
+    <div>
+        <label for="title">Inserisci un prezzo</label>
+        <input type="text"  name="price" placeholder="prezzo"> 
+    </div>
+        <!-- serie -->
+    <div>
+        <label for="title">Inserisci la serie</label>
+        <input type="text"  name="series" placeholder="serie">
+    </div>
+        <!-- data -->
+        <div>
+        <label for="title">Inserisci la data di pubblicazione</label>
+        <input type="date"  name="date" placeholder="data di pubblicazione">
+    </div>
+        <!-- genere -->
+        <div>
+        <label for="title">Inserisci il genere </label>
+        <input type="text"  name="type" placeholder="genere">
+    </div>
+</form>
 </body>
 </html>
