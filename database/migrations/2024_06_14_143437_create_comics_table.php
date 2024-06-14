0095a10dde9bbcc30a0ec2b85354b07b98576926
Om12ek99 <omar.elkamili@gmail.com>
2024-06-14 16:39:22 +0200

    add: commen of MySql

--- database/migrations/2024_06_14_143437_create_comics_table.php
+++ database/migrations/2024_06_14_143437_create_comics_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('comics', function (Blueprint $table) {
+        Schema::create('comics', function (Blueprint $table) {          //Crea una nuova tabella chiamata comics nel database. La funzione accetta una callback che riceve un'istanza di Blueprint utilizzata per definire le colonne della tabella.
             $table->id();
             $table->string('title');
             $table->string('author');
@@ -22,6 +22,20 @@ return new class extends Migration
             $table->timestamps();
         });
     }
+
+
+    //in MySql
+    // CREATE TABLE `comics` (
+    //     `id` bigint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    //     `title` varchar(255) NOT NULL,
+    //     `author` varchar(255) NOT NULL,
+    //     `description` text NULL,
+    //     `publisher` varchar(255) NOT NULL,
+    //     `release_date` date NOT NULL,
+    //     `price` decimal(8, 2) NOT NULL,
+    //     `created_at` timestamp NULL DEFAULT NULL,
+    //     `updated_at` timestamp NULL DEFAULT NULL
+    //   );
 
     /**
      * Reverse the migrations.
