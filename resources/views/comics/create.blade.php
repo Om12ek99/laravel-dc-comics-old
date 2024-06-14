956d56cc71e7e095e25e8aa486ad2ca6a0b9a3c3
Om12ek99 <omar.elkamili@gmail.com>
2024-06-14 21:21:21 +0200

    add: added layout and changed form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,51 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>crea</h1>
-    <form 
-    method="post"
-    action="{{route('comics.store')}}">
-    @csrf
-    @method('post')
+@extends('layouts.app')
 
-    <!-- title -->
-    <div>
-        <label for="title">Inserisci un titolo</label>
-        <input type="text"  name="title" placeholder="titolo">
-    </div>
-    <!-- description -->
-    <div>
-        <label for="desc">Inserisci una descrizione</label>
-        <input type="text"  name="desc" placeholder="descrizione">
-    </div>
-    <!-- prezzo -->
-    <div>
-        <label for="price">Inserisci un prezzo</label>
-        <input type="text"  name="price" placeholder="prezzo"> 
-    </div>
-        <!-- serie -->
-    <div>
-        <label for="title">Inserisci la serie</label>
-        <input type="text"  name="series" placeholder="serie">
-    </div>
-        <!-- data -->
+@section('content')
+    <h1>Create Comic</h1>
+    <form action="{{ route('comics.store') }}" method="POST">
+        @csrf
         <div>
-        <label for="date">Inserisci la data di pubblicazione</label>
-        <input type="date"  name="date" placeholder="data di pubblicazione">
-    </div>
-        <!-- genere -->
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title" required>
+        </div>
         <div>
-        <label for="type">Inserisci il genere </label>
-        <input type="text"  name="type" placeholder="genere">
-    </div>
-    <div>
-        <input type="submit" value="salva modifiche">
-    </div>
-</form>
-</body>
-</html>
+            <label for="description">Description</label>
+            <textarea name="description" id="description" required></textarea>
+        </div>
+        <div>
+            <label for="thumb">Thumb URL</label>
+            <input type="text" name="thumb" id="thumb" required>
+        </div>
+        <div>
+            <label for="price">Price</label>
+            <input type="text" name="price" id="price" required>
+        </div>
+        <div>
+            <label for="series">Series</label>
+            <input type="text" name="series" id="series" required>
+        </div>
+        <div>
+            <label for="sale_date">Sale Date</label>
+            <input type="date" name="sale_date" id="sale_date" required>
+        </div>
+        <div>
+            <label for="type">Type</label>
+            <input type="text" name="type" id="type" required>
+        </div>
+        <button type="submit">Create</button>
+    </form>
+@endsection
