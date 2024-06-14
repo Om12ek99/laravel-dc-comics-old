bbdade96a584bcf907269c6fbb4a5fcfbc8016ee
Om12ek99 <omar.elkamili@gmail.com>
2024-06-14 18:25:41 +0200

    add: bug fixes for route

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ComicController extends Controller
 {
 public function index(){
-    return view("home.index");
+    return view("comics.index");
 }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
 
-Route::get('/home', [ComicController::class,'index'])->name('home.index');
+
+Route::get('/comics', [ComicController::class,'index'])->name('comics.index');
